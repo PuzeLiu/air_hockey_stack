ae96df915efd413768a366a1117a988ff050c366
Puze Liu <lpztc@163.com>
2021-01-01 00:54:19 +0100

    Use iiwas_control in air_hockey package

--- tactical_agent/include/planner/bezier_hit.h
+++ tactical_agent/include/planner/bezier_hit.h
@@ -2,10 +2,9 @@
 // Created by puze on 08.12.20.
 //
 
-#ifndef SRC_BEZIERHIT_H
-#define SRC_BEZIERHIT_H
+#ifndef SRC_BEZIER_HIT_H
+#define SRC_BEZIER_HIT_H
 
-#include "ros/ros.h"
 #include <Eigen/Core>
 #include "trajectory_msgs/MultiDOFJointTrajectory.h"
 #include "trajectory_msgs/MultiDOFJointTrajectoryPoint.h"
@@ -16,7 +15,7 @@ using namespace Eigen;
 namespace tactical_agent {
     class BezierHit {
     public:
-        BezierHit(Vector2d bound_lower, Vector2d bound_upper, double rate, double height = 0.);
+        BezierHit(Vector2d bound_lower, Vector2d bound_upper, double rate, double height);
 
         bool plan(const Vector2d &xStart, const Vector2d &xHit, const Vector2d &vHit,
                   trajectory_msgs::MultiDOFJointTrajectory &cartTraj);
@@ -30,23 +29,21 @@ namespace tactical_agent {
 
         bool getPoint(double t);
 
-
-    public:
+    private:
         Vector2d boundLower_;
         Vector2d boundUpper_;
-    private:
-        Vector2d xStart_, xMiddle_, xHit_, vHit_;    //Start point, middle point, final point, final velocity
+        Vector2d xStart_, xMiddle_, xHit_, vHit_, vHitNormalized_;    //Start point, middle point, final point, final velocity
         double rate_;
         double height_;
         double tHit_, tStop_;
+        double aMax_;
         double z, dz_dt_, dz_ddt_;        //Phase position, velocity, acceleration
         Vector2d dx_dz_, dx_ddz_;       //Catesian velocity and acceleration w.r.t phase
         Vector2d x_, dx_dt_, dx_ddt_;    //Catesian velocity and acceleration w.r.t time
         Matrix<double, 5, 1> phaseCoeff_;
 
-        Vector2d xMiddleStop_;
         trajectory_msgs::MultiDOFJointTrajectoryPoint viaPoint_;
     };
 }
 
-#endif //SRC_BEZIERHIT_H
+#endif //SRC_BEZIER_HIT_H
--- tactical_agent/src/planner/bezier_hit.cpp
+++ tactical_agent/src/planner/bezier_hit.cpp
@@ -1,4 +1,4 @@
-#include "planner/bezierHit.h"
+#include "planner/bezier_hit.h"
 
 using namespace tactical_agent;
 
@@ -7,6 +7,7 @@ BezierHit::BezierHit(Vector2d bound_lower, Vector2d bound_upper, double rate, do
     boundUpper_ = bound_upper;
     rate_ = rate;
     height_ = height;
+    aMax_ = 5.;
 
     viaPoint_.transforms.resize(1);
     viaPoint_.velocities.resize(1);
@@ -26,6 +27,7 @@ bool BezierHit::plan(const Vector2d &xStart, const Vector2d &xHit, const Vector2
             cartTraj.points.push_back(viaPoint_);
         }
     }
+    cartTraj.header.stamp = ros::Time::now();
     return true;
 }
 
@@ -35,7 +37,6 @@ bool BezierHit::fit(const Vector2d& x0, const Vector2d& xf, const Vector2d& vf) 
         if (x0[i] < boundLower_[i] or x0[i] > boundUpper_[i] or
             xf[i] < boundLower_[i] or xf[i] > boundUpper_[i] or
             vf.norm() == 0.) {
-            ROS_INFO_STREAM(x0[i] << " " << xf[i] << " "<< boundLower_[i] << " " << boundUpper_[i]);
             cout << "Planner Failed: points out of boundary!" << endl;
             return false;
         }
@@ -44,28 +45,24 @@ bool BezierHit::fit(const Vector2d& x0, const Vector2d& xf, const Vector2d& vf) 
     xStart_ = x0;
     xHit_ = xf;
     vHit_ = vf;
+    vHitNormalized_ = vHit_.normalized();
     // Find Middle Point in hitting
     double alpha_min_hit = INFINITY;        // cross point at hitting phase
     for (int i = 0; i < 2; ++i) {
         if (vHit_[i] > 0) {
             alpha_min_hit = min(alpha_min_hit, (xHit_[i] - boundLower_[i]) / vHit_[i]);
-        } else if (vf[i] < 0) {
+        } else if (vHit_[i] < 0) {
             alpha_min_hit = min(alpha_min_hit, (xHit_[i] - boundUpper_[i]) / vHit_[i]);
         }
     }
     xMiddle_ = xHit_ - alpha_min_hit * vHit_;
-    xMiddleStop_ = xHit_ + alpha_min_hit * vHit_;
 
     // Construct Quartic Polynomial
     double dz_dtf;
-    if (vHit_[0] != 0.){
-        dz_dtf = vHit_[0] / (xHit_[0] - xMiddle_[0]) / 2;
-    } else {
-        dz_dtf = vHit_[1] / (xHit_[1] - xMiddle_[1]) / 2;
-    }
+    dz_dtf = vHit_.norm() / (xHit_ - xMiddle_).norm() / 2;
 
     tHit_ = 2 / dz_dtf;
-    tStop_ = 3 / dz_dtf;
+    tStop_ = vHit_.norm() / aMax_ + tHit_;
     phaseCoeff_[0] = 0.;
     phaseCoeff_[1] = 0.;
     phaseCoeff_[2] = 0.;
@@ -87,15 +84,9 @@ bool BezierHit::getPoint(double t) {
         dx_ddt_ = dx_ddz_ * pow(dz_dt_, 2) + dx_dz_ * dz_ddt_;
     }
     else if (t <= tStop_){
-        z = phaseCoeff_[3] * pow(t, 3) + phaseCoeff_[4] * pow(t, 4) - 1;
-        dz_dt_ = 3 * phaseCoeff_[3] * pow(t, 2) + 4 * phaseCoeff_[4] * pow(t, 3);
-        dz_ddt_ = 6 * phaseCoeff_[3] * t + 12 * phaseCoeff_[4] * pow(t, 2);
-
-        x_ = xMiddleStop_ + pow((1 - z), 2) * (xHit_ - xMiddleStop_) + pow(z, 2) * (xStart_ - xMiddleStop_) ;
-        dx_dz_ = (2 * (1 - z) * (xMiddleStop_ - xHit_) + 2 * z * (xStart_ - xMiddleStop_));
-        dx_dt_ = dx_dz_ * dz_dt_;
-        dx_ddz_ = 2 * (xHit_ - 2 * xMiddleStop_ + xStart_);
-        dx_ddt_ = dx_ddz_ * pow(dz_dt_, 2) + dx_dz_ * dz_ddt_;
+        dx_ddt_ = -aMax_ * vHitNormalized_;
+        dx_dt_ = vHit_ - aMax_ * vHitNormalized_ * (t - tHit_);
+        x_ = xHit_ + vHit_ * (t - tHit_) - 0.5 * aMax_  * vHitNormalized_ * pow((t-tHit_), 2);
     }
     else{
         return false;
