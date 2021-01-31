f248982dfdb9eded7ba79390e3bcc9da959fa115
Puze Liu <lpztc@163.com>
2021-01-31 14:20:18 +0100

    Add namespace for puck tf publisher

--- air_hockey_gazebo/src/puck_tf_publisher_plugin.cc
+++ air_hockey_gazebo/src/puck_tf_publisher_plugin.cc
@@ -3,8 +3,6 @@
 using namespace gazebo;
 
 PuckTFPublisher::PuckTFPublisher() {
-    transformMsg_.header.frame_id = "world";
-    transformMsg_.child_frame_id = "Puck";
     update_rate_ = 0.;
     update_period_ = 0.;
 }
@@ -33,7 +31,11 @@ void PuckTFPublisher::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
     } else {
         this->robot_namespace_ = _sdf->GetElement ( "robotNamespace" )->Get<std::string>();
     }
-    rosnode_ = boost::shared_ptr<ros::NodeHandle> ( new ros::NodeHandle ( this->robot_namespace_ ) );
+    rosnode_ = boost::shared_ptr<ros::NodeHandle> ( new ros::NodeHandle (this->robot_namespace_) );
+
+    rosnode_->param<std::string>("parent_frame_id", transformMsg_.header.frame_id, "world");
+    rosnode_->param<std::string>("child_frame_id", transformMsg_.child_frame_id, "Puck");
+    ROS_INFO_STREAM(rosnode_->getNamespace() << " ########### TF ID:" << transformMsg_.child_frame_id);
 
     this->update_rate_ = 100.0;
     if (_model->GetSDF()->HasElement("updateRate")){
