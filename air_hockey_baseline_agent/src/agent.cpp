527c2e1f81fcf40a3771f108e540a3c1dcba7f74
Puze Liu <puze@robot-learning.de>
2021-02-08 18:38:33 +0100

    Change the height of the puck

--- air_hockey_baseline_agent/src/agent.cpp
+++ air_hockey_baseline_agent/src/agent.cpp
@@ -251,7 +251,7 @@ void Agent::gotoHome(bool restart) {
 
 double Agent::updateGoal(Vector2d puckPosition) {
     auto random_integer = dist_(rng_);
-//    random_integer = 0;
+    random_integer = 0;
     if (puckPosition(1) > 0.1) {
         xGoal_.x() = tableLength_;
         xGoal_.y() = -0.1;
