8a75b84af51f6592f809f8cc9fd8e74ae80ac4eb
sieburger <verena.sieburger@stud.tu-darmstadt.de>
2021-06-22 10:12:49 +0200

    clean code - no changes

--- air_hockey_puck_tracker/src/EKF_Wrapper.cpp
+++ air_hockey_puck_tracker/src/EKF_Wrapper.cpp
@@ -2,7 +2,7 @@
 
 using namespace air_hockey_baseline_agent;
 
-const PuckState & EKF_Wrapper::update(ObservationModel &m, const Measurement &z) {
+const PuckState& EKF_Wrapper::update(ObservationModel& m, const Measurement& z) {
     m.updateJacobians(x);
 
     // COMPUTE KALMAN GAIN
@@ -24,18 +24,17 @@ const PuckState & EKF_Wrapper::update(ObservationModel &m, const Measurement &z)
     return x;
 }
 
-const air_hockey_baseline_agent::EKF_Wrapper::InnovationCovariance &
-air_hockey_baseline_agent::EKF_Wrapper::updateInnovationCovariance(ObservationModel &m) {
+const EKF_Wrapper::InnovationCovariance& EKF_Wrapper::updateInnovationCovariance(ObservationModel &m) {
     S = (m.H * P * m.H.transpose()) + (m.V * m.getCovariance() * m.V.transpose());
     return S;
 }
 
-void air_hockey_baseline_agent::EKF_Wrapper::calculateInnovation(ObservationModel &m, const Measurement &z) {
+void EKF_Wrapper::calculateInnovation(ObservationModel &m, const Measurement &z) {
     mu = z - m.h(x);
     Eigen::Rotation2Dd rot(mu.theta());
     mu.theta() = rot.smallestAngle();
 }
 
-void air_hockey_baseline_agent::EKF_Wrapper::moveOneStep(SystemModel &s, const Control &u) {
+void EKF_Wrapper::moveOneStep(SystemModel &s, const Control &u) {
 	x = s.f(x, u);
 }
