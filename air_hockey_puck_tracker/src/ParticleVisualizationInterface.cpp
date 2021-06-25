a362e9a98f98685cd7bd13257f0a96a5c79b5d4e
sieburger <verena.sieburger@stud.tu-darmstadt.de>
2021-06-25 11:47:27 +0200

    improve visualisation performance

--- air_hockey_puck_tracker/src/ParticleVisualizationInterface.cpp
+++ air_hockey_puck_tracker/src/ParticleVisualizationInterface.cpp
@@ -8,40 +8,30 @@
 using namespace air_hockey_baseline_agent;
 
 ParticleVisualizationInterface::ParticleVisualizationInterface(const ros::NodeHandle &nh): m_nh(nh) {
-    m_particlePub = m_nh.advertise<visualization_msgs::MarkerArray> ("visualization_marker_array", 10);
+    m_particlePub = m_nh.advertise<visualization_msgs::Marker> ("visualization_particle", 1);
 }
 
 void ParticleVisualizationInterface::visualize(ParticleFilter& particleFilter, const std::string& tableRefName) {
-    ROS_INFO_STREAM("Visualize Particles of Particle Filter");
-    for (int i= 0; i < particleFilter.getParticles().size(); i++){
-        PuckState state = particleFilter.getParticles().at(i);
-        visualization_msgs::Marker marker;
-        marker.header.frame_id = tableRefName;
-        marker.header.stamp = ros::Time();
-        marker.ns = "Particle";
-        marker.id = i;
-        marker.type = visualization_msgs::Marker::SPHERE;
-        marker.action = visualization_msgs::Marker::MODIFY;
-        marker.scale.x = 0.02;
-        marker.scale.y = 0.02;
-        marker.scale.z = 0.02;
-        marker.color.r = 1.0;
-        marker.color.g = 1.0;
-        marker.color.b = 0.0;
-        marker.color.a = 1.0;
-        marker.pose.position.x = state.x();
-        marker.pose.position.y = state.y();
-        tf2::Quaternion quaternion;
-        quaternion.setRPY(0., 0., state.theta());
-        marker.pose.orientation.x = quaternion.x();
-        marker.pose.orientation.y = quaternion.y();
-        marker.pose.orientation.z = quaternion.z();
-        marker.pose.orientation.w = quaternion.w();
-        // lifetime duration in sec
-        //marker.lifetime = ros::Duration(5);
+    m_marker.header.frame_id = tableRefName;
+    m_marker.header.stamp = ros::Time();
+    m_marker.ns = "Particles";
+    m_marker.type = visualization_msgs::Marker::SPHERE_LIST;
+    m_marker.action = visualization_msgs::Marker::MODIFY;
+    m_marker.scale.x = 0.02;
+    m_marker.scale.y = 0.02;
+    m_marker.scale.z = 0.02;
+    m_marker.color.r = 1.0;
+    m_marker.color.g = 1.0;
+    m_marker.color.b = 0.0;
+    m_marker.color.a = 1.0;
 
-        m_particleMarkerMsg.markers.push_back(marker);
+    for (PuckState state: particleFilter.getParticles()){
+        geometry_msgs::Point point;
+        point.x = state.x();
+        point.y = state.y();
+        m_marker.points.push_back(point);
 
     }
-    m_particlePub.publish(m_particleMarkerMsg);
+    m_particlePub.publish(m_marker);
+    m_marker.points.clear();
 }
