f1a37a04bae171784508587575dec3942cb83d4b
Puze Liu <lpztc@163.com>
2022-01-14 20:14:31 +0100

    Add air_hockey_pybullet package
    - fixed minor issues in striker urdf

---
+++ air_hockey_pybullet/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# for your packages to be recognized by python
+d = generate_distutils_setup(
+ packages=['air_hockey_pybullet'],
+ package_dir={'': 'src'}
+)
+
+setup(**d)
