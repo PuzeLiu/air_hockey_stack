e664fe37a43eb3ebde1f8ef960139e5b0d309d0b
Puze Liu <puze@robot-learning.de>
2022-04-25 20:20:40 +0200

    Add testing scripts

---
+++ air_hockey_baseline_agent/scripts/hit_test/plot_record.py
@@ -0,0 +1,69 @@
+import os.path
+
+import numpy as np
+import matplotlib.pyplot as plt
+import rosbag
+
+import pinocchio as pino
+
+
+def read_bag(bag, duration):
+    time = []
+    joint_state_desired = []
+    joint_state_actual = []
+    joint_state_error = []
+
+    for topic, msg, t_bag in bag.read_messages():
+        if topic in ["/iiwa_front/adrc_trajectory_controller/state"]:
+            n_joints = len(msg.joint_names)
+            for i in range(n_joints):
+                time.append(msg.header.stamp.to_sec())
+                joint_state_desired.append(np.concatenate([msg.desired.positions, msg.desired.velocities, msg.desired.accelerations]))
+                joint_state_actual.append(np.concatenate([msg.actual.positions, msg.actual.velocities, msg.actual.accelerations]))
+                joint_state_error.append(np.concatenate([msg.error.positions, msg.error.velocities, msg.error.accelerations]))
+
+    return np.array(time), np.array(joint_state_desired), np.array(joint_state_actual), np.array(joint_state_error)
+
+
+root_dir = os.path.dirname(__file__)
+bag_file = rosbag.Bag(os.path.join(root_dir, "2022-04-25-19-22-29.bag"))
+
+robot_file = os.path.join(root_dir, "iiwa_striker.urdf")
+pino_model = pino.buildModelFromUrdf(robot_file)
+pino_data = pino_model.createData()
+pino_positions = np.zeros(pino_model.nq)
+
+t, desired, actual, error = read_bag(bag_file, 10000)
+
+ee_pos_des = np.zeros((t.shape[0], 3))
+ee_pos_actual = np.zeros((t.shape[0], 3))
+frame_id = pino_model.getFrameId("F_striker_joint_link")
+for i, _ in enumerate(t):
+    pino_positions[:7] = desired[i, :7]
+    pino.forwardKinematics(pino_model, pino_data, pino_positions)
+    pino.updateFramePlacement(pino_model, pino_data, frame_id)
+    ee_pos_des[i] = pino_data.oMf[24].translation.copy()
+
+    pino_positions[:7] = actual[i, :7]
+    pino.forwardKinematics(pino_model, pino_data, pino_positions)
+    pino.updateFramePlacement(pino_model, pino_data, frame_id)
+    ee_pos_actual[i] = pino_data.oMf[24].translation.copy()
+
+fig, axes = plt.subplots(4, 2)
+for i in range(7):
+    # axes[int(i/2), i%2].plot(desired[:, i])
+    # axes[int(i/2), i%2].plot(actual[:, i])
+    axes[int(i/2), i % 2].plot(error[:, i])
+
+    axes[int(i/2), i%2].set_title("joint_{}".format(i+1))
+
+fig, axes = plt.subplots(3)
+for i in range(3):
+    axes[i].plot(t, ee_pos_des[:, i])
+    axes[i].plot(t, ee_pos_actual[:, i])
+
+fig, ax = plt.subplots(1)
+ax.plot(ee_pos_des[:, 0], ee_pos_des[:, 1])
+ax.plot(ee_pos_actual[:, 0], ee_pos_actual[:, 1])
+
+plt.show()
---
+++ air_hockey_baseline_agent/scripts/hit_test/publish_piotr_trajectory.py
@@ -0,0 +1,60 @@
+import numpy as np
+import time
+from copy import deepcopy
+import rospy
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+if __name__ == '__main__':
+    # type_name = 'joint_feedforward'
+    type_name = 'adrc'
+    use_front = True
+
+    topic_name = '/iiwa_front/' if use_front else '/iiwa_back/'
+    joint_prefix = 'F' if use_front else 'B'
+
+    rospy.init_node("publish_traj_command", anonymous=True)
+    cmdPub = rospy.Publisher(topic_name + type_name + "_trajectory_controller/command", JointTrajectory, queue_size=1)
+    rospy.sleep(2.0)
+
+    traj_record = np.load("trajectory.npy", allow_pickle=True)[()]
+
+    traj = JointTrajectory()
+
+    traj.joint_names.append(joint_prefix + "_joint_1")
+    traj.joint_names.append(joint_prefix + "_joint_2")
+    traj.joint_names.append(joint_prefix + "_joint_3")
+    traj.joint_names.append(joint_prefix + "_joint_4")
+    traj.joint_names.append(joint_prefix + "_joint_5")
+    traj.joint_names.append(joint_prefix + "_joint_6")
+    traj.joint_names.append(joint_prefix + "_joint_7")
+
+    traj_point_goal = JointTrajectoryPoint()
+    traj_point_goal.positions = np.zeros(7)
+    traj_point_goal.velocities = np.zeros(7)
+    traj_point_goal.accelerations = np.zeros(7)
+
+    traj_point_goal.positions[:6] = traj_record['q'][0]
+    traj_point_goal.time_from_start = rospy.Time(3.0)
+    traj.points.append(traj_point_goal)
+    traj.header.stamp = rospy.Time.now()
+    cmdPub.publish(traj)
+
+    time.sleep(3.0)
+
+    traj.points.clear()
+    for i in range(traj_record['t'].shape[0]):
+        traj_point_goal.positions[:6] = traj_record['q'][i]
+        traj_point_goal.velocities[:6] = traj_record['dq'][i]
+        traj_point_goal.accelerations[:6] = traj_record['ddq'][i]
+        traj_point_goal.time_from_start = rospy.Duration(traj_record['t'][i])
+        traj.points.append(deepcopy(traj_point_goal))
+
+    for i in range(1, traj_record['t_r'].shape[0]):
+        traj_point_goal.positions[:6] = traj_record['q_r'][i]
+        traj_point_goal.velocities[:6] = traj_record['dq_r'][i]
+        traj_point_goal.accelerations[:6] = traj_record['ddq_r'][i]
+        traj_point_goal.time_from_start = rospy.Duration(traj_record['t_r'][i] + traj_record['t'][-1])
+        traj.points.append(deepcopy(traj_point_goal))
+
+    traj.header.stamp = rospy.Time.now() + rospy.Duration(0.1)
+    cmdPub.publish(traj)
