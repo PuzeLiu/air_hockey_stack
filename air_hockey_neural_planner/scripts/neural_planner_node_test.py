c31f75a57d5e111c87b930359cf7bbc53ab9d29a
kicki <piotr.kicki@put.poznan.pl>
2022-06-22 16:56:21 +0200

    Added gazebo switch

--- air_hockey_neural_planner/scripts/neural_planner_node_test.py
+++ air_hockey_neural_planner/scripts/neural_planner_node_test.py
@@ -32,6 +32,7 @@ class NeuralPlannerTestNode:
         rospy.sleep(2.)
         trans, _ = self.tf_listener.lookupTransform('/F_link_0', '/TableAway', rospy.Time(0))
         self.goal = trans
+        self.gazebo = False
 
     def set_robot_state(self, msg):
         self.robot_joint_pose = msg.position[:7]
@@ -44,32 +45,39 @@ class NeuralPlannerTestNode:
             print(puck_goal_y, puck_goal_x)
             th = np.arctan2(puck_goal_y, puck_goal_x)
             print(th)
-            #r = 0.07
-            r = 0.1
+            r = 0.07
+            #r = 0.1
             x = puck_pos[0] - r * np.cos(th)
             y = puck_pos[1] - r * np.sin(th)
             return x, y, th
 
-        state_msg = ModelState()
-        state_msg.model_name = 'puck'
-        state_msg.pose.position.x = -0.5
-        state_msg.pose.position.y = -0.2
-        state_msg.pose.position.z = 0.0
-        state_msg.pose.orientation.x = 0
-        state_msg.pose.orientation.y = 0
-        state_msg.pose.orientation.z = 0
-        state_msg.pose.orientation.w = 0
-        state_msg.reference_frame = "air_hockey_table::Table"
-        rospy.wait_for_service('/gazebo/set_model_state')
-        set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
-        set_state(state_msg)
-        rospy.sleep(0.5)
+        if self.gazebo:
+            state_msg = ModelState()
+            state_msg.model_name = 'puck'
+            state_msg.pose.position.x = -0.5
+            state_msg.pose.position.y = -0.2
+            state_msg.pose.position.z = 0.0
+            state_msg.pose.orientation.x = 0
+            state_msg.pose.orientation.y = 0
+            state_msg.pose.orientation.z = 0
+            state_msg.pose.orientation.w = 0
+            state_msg.reference_frame = "air_hockey_table::Table"
+            rospy.wait_for_service('/gazebo/set_model_state')
+            set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+            set_state(state_msg)
+            rospy.sleep(0.5)
         while not rospy.is_shutdown():
             try:
                 trans, rot = self.tf_listener.lookupTransform('/F_link_0', '/Puck', rospy.Time(0))
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
             print_(trans)
+            if trans[0] < 0.9 or trans[0] > 1.3:
+                print("Puck not in x range")
+                break
+            if trans[1] < -0.4 or trans[1] > 0.4:
+                print("Puck not in y range")
+                break
             x, y, th = get_desired_xyth(trans, self.goal)
             pr = PlannerRequest()
             pr.q_0 = self.robot_joint_pose
@@ -80,6 +88,7 @@ class NeuralPlannerTestNode:
             end_point = Point(x, y, 0.16)
             pr.end_point = end_point
             pr.hitting_angle = th
+            print("Run trajectory")
             self.planner_request_publisher.publish(pr)
             break
 
