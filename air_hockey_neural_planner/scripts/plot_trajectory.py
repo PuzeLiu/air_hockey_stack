55ba3dfd30e329978ff2cdcd5b3124d8459af14b
kicki <piotr.kicki@put.poznan.pl>
2022-07-22 15:33:51 +0200

    Fixed plotting and added filtering

--- air_hockey_neural_planner/scripts/plot_trajectory.py
+++ air_hockey_neural_planner/scripts/plot_trajectory.py
@@ -5,6 +5,31 @@ import matplotlib.pyplot as plt
 import rosbag
 
 import pinocchio as pino
+from scipy.signal import butter, filtfilt
+
+
+def cal_d(fs, dts):
+    dfs = np.zeros_like(fs)
+    dfs[1: -1] = (fs[2:, :] - fs[:-2, :]) / (dts[2:] - dts[:-2])
+    dfs[0] = dfs[1]
+    dfs[-1] = dfs[-2]
+    return dfs
+
+
+def get_vel_acc(t, q_m):
+    # Position
+    b, a = butter(6, 4, fs=1000)
+    q_m_filter = q_m
+
+    # Velocity
+    dq_m = cal_d(q_m, t)
+    dq_m_filter = filtfilt(b, a, dq_m, axis=0)
+
+    # Acceleration
+    ddq_m = cal_d(dq_m_filter, t)
+    ddq_m_filter = filtfilt(b, a, ddq_m, axis=0)
+
+    return q_m_filter, dq_m_filter, ddq_m_filter, q_m, dq_m, ddq_m
 
 
 def read_bag(bag, duration):
@@ -13,19 +38,22 @@ def read_bag(bag, duration):
     joint_state_actual = []
     joint_state_error = []
     puck_pose = []
+    puck_time = []
 
     for topic, msg, t_bag in bag.read_messages():
         if topic in ["/iiwa_front/adrc_trajectory_controller/state",
-                    "/iiwa_front/bspline_adrc_joint_trajectory_controller/state",
+                     "/iiwa_front/bspline_adrc_joint_trajectory_controller/state",
                      "/iiwa_front/joint_feedforward_trajectory_controller/state"]:
             n_joints = len(msg.joint_names)
-            for i in range(n_joints):
-                time.append(msg.header.stamp.to_sec())
-                if len(msg.desired.positions) != 7:
-                    msg.desired.positions += (0.,)
-                joint_state_desired.append(np.concatenate([msg.desired.positions, msg.desired.velocities, msg.desired.accelerations, msg.desired.effort]))
-                joint_state_actual.append(np.concatenate([msg.actual.positions, msg.actual.velocities, msg.actual.accelerations, msg.actual.effort]))
-                joint_state_error.append(np.concatenate([msg.error.positions, msg.error.velocities, msg.error.accelerations, msg.error.effort]))
+            time.append(msg.header.stamp.to_sec())
+            if len(msg.desired.positions) != 7:
+                msg.desired.positions += (0.,)
+            joint_state_desired.append(np.concatenate(
+                [msg.desired.positions, msg.desired.velocities, msg.desired.accelerations, msg.desired.effort]))
+            joint_state_actual.append(np.concatenate(
+                [msg.actual.positions, msg.actual.velocities, msg.actual.accelerations, msg.actual.effort]))
+            joint_state_error.append(np.concatenate(
+                [msg.error.positions, msg.error.velocities, msg.error.accelerations, msg.error.effort]))
         elif topic == "/tf":
             data = msg.transforms[0]
             frame_id = data.child_frame_id
@@ -35,90 +63,95 @@ def read_bag(bag, duration):
                 dy = 0.813
                 dz = 0.03
                 puck_pose.append([t.x - dx, t.y - dy, t.z - dz])
+                puck_time.append(data.header.stamp.to_sec())
 
-
-
-    return np.array(time), np.array(joint_state_desired), np.array(joint_state_actual), np.array(joint_state_error),\
-           np.array(puck_pose)
+    return np.array(time), np.array(joint_state_desired), np.array(joint_state_actual), np.array(joint_state_error), \
+           np.array(puck_pose), np.array(puck_time)
 
 
 root_dir = os.path.dirname(__file__)
 package_dir = os.path.dirname(root_dir)
-#bag_path = os.path.join(package_dir, "test_bspline_adrc_ff.bag")
-bag_path = os.path.join(package_dir, "bags/qddotend.bag")
+# bag_path = os.path.join(package_dir, "test_bspline_adrc_ff.bag")
+# bag_path = os.path.join(package_dir, "bags/qddotend.bag")
+bag_path = os.path.join(package_dir, "tilted_c1_t1.bag")
+
 bag_file = rosbag.Bag(bag_path)
 
-#robot_file = os.path.join(root_dir, "manifold_planning", "iiwa_striker_new.urdf")
-robot_file = os.path.join(root_dir, "manifold_planning", "iiwa.urdf")
+robot_file = os.path.join(root_dir, "manifold_planning", "iiwa_striker_new.urdf")
+# robot_file = os.path.join(root_dir, "manifold_planning", "iiwa.urdf")
 pino_model = pino.buildModelFromUrdf(robot_file)
 pino_data = pino_model.createData()
 pino_positions = np.zeros(pino_model.nq)
 pino_velocities = np.zeros(pino_model.nq)
 
-t, desired, actual, error, puck = read_bag(bag_file, 10000)
+t, desired, actual, error, puck, puck_time = read_bag(bag_file, 10000)
 
-applied_torque_estimate = []
+q, dq, ddq, q_, dq_, ddq_ = get_vel_acc(t[:, np.newaxis], actual[:, :7])
+
+# applied_torque_estimate = []
 ee_pos_des = np.zeros((t.shape[0], 3))
 ee_pos_actual = np.zeros((t.shape[0], 3))
-frame_id = 18#pino_model.getFrameId("F_striker_joint_link")
+# frame_id = pino_model.getFrameId("F_striker_joint_link")
+frame_id = pino_model.getFrameId("F_striker_mallet")
 for i, _ in enumerate(t):
     pino_positions[:7] = desired[i, :7]
     pino.forwardKinematics(pino_model, pino_data, pino_positions)
     pino.updateFramePlacement(pino_model, pino_data, frame_id)
-    ee_pos_des[i] = pino_data.oMf[-1].translation.copy()
+    ee_pos_des[i] = pino_data.oMf[frame_id].translation.copy()
 
     pino_positions[:7] = actual[i, :7]
     pino.forwardKinematics(pino_model, pino_data, pino_positions)
     pino.updateFramePlacement(pino_model, pino_data, frame_id)
-    ee_pos_actual[i] = pino_data.oMf[-1].translation.copy()
+    ee_pos_actual[i] = pino_data.oMf[frame_id].translation.copy()
 
-    pino_velocities[:7] = error[i, 7:14]
-    g_and_c = pino.nonLinearEffects(pino_model, pino_data, pino_positions, pino_velocities)
-    a = 0
-    applied_torque_estimate.append(desired[i, 21:28] + g_and_c)
+# detect hitting idx
+vxpuck = np.diff(puck[:, 0], axis=0)
+vypuck = np.diff(puck[:, 0], axis=0)
+vpuck = np.sqrt(vxpuck ** 2 + vypuck ** 2)
+hit_idx = np.where(vpuck > 0.01)[0][0]
+puck_time_hit = puck_time[hit_idx] - puck_time[0]
+time_zeroed = t - t[0]
+time_hit_idx = np.argmin(np.abs(puck_time_hit - time_zeroed))
 
-    #J = pino.computeFrameJacobian(pino_model, pino_data, actual[i, :9], frame_id, pino.LOCAL_WORLD_ALIGNED)
-applied_torque_estimate = np.stack(applied_torque_estimate, axis=0)
-for i in range(6):
-    plt.subplot(231+i)
-    plt.plot(t, applied_torque_estimate[:, i], label=f"estimated_effort_{i}")
-    plt.plot(t, actual[:, 21+i], label=f"measured_effort_{i}")
-    plt.legend()
+plt.plot(ee_pos_des[:, 0], ee_pos_des[:, 1], 'b', label="desired")
+plt.plot(ee_pos_actual[:, 0], ee_pos_actual[:, 1], 'r', label="actual")
+plt.plot(puck[:, 0], puck[:, 1], 'g', label="puck")
 plt.show()
 
-plt.plot(ee_pos_des[:, 0], ee_pos_des[:, 1], 'b')
-plt.plot(ee_pos_actual[:, 0], ee_pos_actual[:, 1], 'r')
-plt.plot(puck[:, 0], puck[:, 1], 'g')
-plt.show()
+fig_1, axes_1 = plt.subplots(3, 2)
+fig_2, axes_2 = plt.subplots(3, 2)
+fig_3, axes_3 = plt.subplots(3, 2)
+fig_4, axes_4 = plt.subplots(3, 2)
+fig_1.suptitle("Positions errors")
+fig_2.suptitle("Positions")
+fig_3.suptitle("Velocities")
+fig_4.suptitle("Accelerations")
+
+
+def plot_actual_desired(ax, i, actual, desired):
+    ax[int(i / 2), i % 2].plot(t, actual[:, i], label="actual")
+    ax[int(i / 2), i % 2].plot(t[time_hit_idx], actual[time_hit_idx, i], 'rx')
+    ax[int(i / 2), i % 2].plot(t, desired[:, i], label="desired")
+    ax[int(i / 2), i % 2].plot(t[time_hit_idx], desired[time_hit_idx, i], 'rx')
+    ax[int(i / 2), i % 2].legend()
+    ax[int(i / 2), i % 2].set_title("joint_{}".format(i + 1))
 
 
 for i in range(6):
-    plt.plot(t, actual[:, 21+i:22+i], label=f"act_effort_{i}")
-    #plt.plot(error[:, 21+i:22+i], label=f"err_effort_{i}")
-plt.legend()
-plt.show()
-
-for i in range(6):
-    plt.plot(t, desired[:, 14+i:15+i], label=f"desired_acc_{i}")
-    #plt.plot(error[:, 21+i:22+i], label=f"err_effort_{i}")
-plt.legend()
-
-
-fig, axes = plt.subplots(4, 2)
-for i in range(7):
-    #axes[int(i/2), i%2].plot(desired[:, i])
-    # axes[int(i/2), i%2].plot(actual[:, i])
-    axes[int(i/2), i % 2].plot(t, error[:, i])
-
-    axes[int(i/2), i%2].set_title("joint_{}".format(i+1))
-
-fig, axes = plt.subplots(3)
-for i in range(3):
-    axes[i].plot(t, ee_pos_des[:, i])
-    axes[i].plot(t, ee_pos_actual[:, i])
-
-fig, ax = plt.subplots(1)
-ax.plot(ee_pos_des[:, 0], ee_pos_des[:, 1])
-ax.plot(ee_pos_actual[:, 0], ee_pos_actual[:, 1])
-
+    axes_1[int(i / 2), i % 2].plot(t, error[:, i])
+    axes_1[int(i / 2), i % 2].plot(t[time_hit_idx], error[time_hit_idx, i], 'rx')
+    axes_1[int(i / 2), i % 2].set_title("joint_{}".format(i + 1))
+    plot_actual_desired(axes_2, i, q, desired[:, :7])
+    plot_actual_desired(axes_3, i, dq, desired[:, 7:14])
+    plot_actual_desired(axes_4, i, ddq, desired[:, 14:21])
+#
+# fig, axes = plt.subplots(3)
+# for i in range(3):
+#    axes[i].plot(t, ee_pos_des[:, i])
+#    axes[i].plot(t, ee_pos_actual[:, i])
+#
+# fig, ax = plt.subplots(1)
+# ax.plot(ee_pos_des[:, 0], ee_pos_des[:, 1])
+# ax.plot(ee_pos_actual[:, 0], ee_pos_actual[:, 1])
+#
 plt.show()
