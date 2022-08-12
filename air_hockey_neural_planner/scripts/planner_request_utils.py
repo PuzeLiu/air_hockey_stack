974d649c014a40b7aa0c667c0f08ea903e18c59b
kicki <piotr.kicki@put.poznan.pl>
2022-08-12 17:16:01 +0200

    Updated planner request

--- air_hockey_neural_planner/scripts/planner_request_utils.py
+++ air_hockey_neural_planner/scripts/planner_request_utils.py
@@ -14,5 +14,6 @@ def unpack_planner_request(msg):
     y_end = end_point.y
     th_hit = msg.hit_angle
     expected_time = msg.expected_time
+    expected_velocity = msg.expected_velocity
     tactic = msg.tactic
-    return tactic, x_hit, y_hit, th_hit, q_0, q_dot_0, q_ddot_0, x_end, y_end, expected_time
+    return tactic, x_hit, y_hit, th_hit, q_0, q_dot_0, q_ddot_0, x_end, y_end, expected_time, expected_velocity
